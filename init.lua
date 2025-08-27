f9b6119573313077908ae59953c83c9752def096
Sengoku11 <batyr.sengoku@gmail.com>
2025-08-27 19:05:36 +0800

    feat: custom clipboard to resolve yanking into system clipboard by ssh

--- init.lua
+++ init.lua
@@ -116,6 +116,27 @@ vim.o.showmode = false
 --  See `:help 'clipboard'`
 vim.schedule(function()
   vim.o.clipboard = 'unnamedplus'
+
+  -- Enable this block when running Neovim over SSH.
+  -- It routes yanks through OSC52 for clipboard sync (fast, no lag),
+  -- while leaving paste operations local for normal performance.
+  -- You'll still be able to paste from your system clipboard with cmd+v or ctrl+shift+v.
+
+  -- local function paste()
+  --   local s = vim.fn.getreg '"'
+  --   local t = vim.fn.getregtype '"'
+  --   return { vim.split(s, '\n', { plain = true }), t }
+  -- end
+
+  -- vim.g.clipboard = {
+  --   name = 'osc52-only-yank',
+  --   copy = {
+  --     ['+'] = require('vim.ui.clipboard.osc52').copy '+',
+  --     ['*'] = require('vim.ui.clipboard.osc52').copy '*',
+  --   },
+  --   paste = { ['+'] = paste, ['*'] = paste },
+  --   cache_enabled = 0,
+  -- }
 end)
 
 -- Enable break indent

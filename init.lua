a032a6cf6b80a0eacf2a249e0129f2864ad7c566
Sengoku11 <batyr.sengoku@gmail.com>
2025-09-06 18:24:24 +0800

    feat: enable friendly snippets

--- init.lua
+++ init.lua
@@ -770,15 +770,12 @@ require('lazy').setup({
           return 'make install_jsregexp'
         end)(),
         dependencies = {
-          -- `friendly-snippets` contains a variety of premade snippets.
-          --    See the README about individual language/framework/plugin snippets:
-          --    https://github.com/rafamadriz/friendly-snippets
-          -- {
-          --   'rafamadriz/friendly-snippets',
-          --   config = function()
-          --     require('luasnip.loaders.from_vscode').lazy_load()
-          --   end,
-          -- },
+          { -- `friendly-snippets` contains a variety of premade snippets.
+            'rafamadriz/friendly-snippets',
+            config = function()
+              require('luasnip.loaders.from_vscode').lazy_load()
+            end,
+          },
         },
         opts = {},
       },

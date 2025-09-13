a19e051cccb19a4535d67821a1d395b973ddcd39
Sengoku11 <batyr.sengoku@gmail.com>
2025-09-13 13:26:17 +0800

    refactor: remove the last plugin from init.lua

--- init.lua
+++ init.lua
@@ -148,6 +148,7 @@ require('lazy').setup({
   { import = 'plugins.editor' },
   { import = 'plugins.coding' },
   { import = 'plugins.lang' },
+  { import = 'plugins.uncategorized' },
 
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
@@ -179,42 +180,7 @@ require('lazy').setup({
   --
   -- Then, because we use the `opts` key (recommended), the configuration runs
   -- after the plugin has been loaded as `require(MODULE).setup(opts)`.
-
-  { -- Collection of various small independent plugins/modules
-    'echasnovski/mini.nvim',
-    config = function()
-      -- Better Around/Inside textobjects
-      --
-      -- Examples:
-      --  - va)  - Visually select Around )paren
-      --  - yinq - Yank Inside Next Quote
-      --  - ci'  - Change Inside 'quote
-      require('mini.ai').setup { n_lines = 500 }
-
-      -- Add/delete/replace surroundings (brackets, quotes, etc.)
-      --
-      -- - saiw) - Surround Add Inner Word )Paren
-      -- - sd'   - Surround Delete 'quotes
-      -- - sr)'  - Surround Replace ) '
-      require('mini.surround').setup()
-
-      -- Simple and easy statusline.
-      --  You could remove this setup call if you don't like it,
-      --  and try some other statusline plugin
-      local statusline = require 'mini.statusline'
-      -- set use_icons to true if you have a Nerd Font
-      statusline.setup { use_icons = vim.g.have_nerd_font }
-
-      -- You can configure sections in the statusline by overriding their
-      -- default behavior. For example, here we set the section for
-      -- cursor location to LINE:COLUMN
-      ---@diagnostic disable-next-line: duplicate-set-field
-      statusline.section_location = function()
-        return '%2l:%-2v'
-      end
-    end,
-  },
-
+  --
   -- For additional information with loading, sourcing and examples see `:help lazy.nvim-🔌-plugin-spec`
   -- Or use telescope!
   -- In normal mode type `<space>sh` then write `lazy.nvim-plugin`

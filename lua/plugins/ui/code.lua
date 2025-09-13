186fca00a487e3ee194a9411da3b737a7b415720
Sengoku11 <batyr.sengoku@gmail.com>
2025-09-13 13:19:27 +0800

    chore: search keys to todo-comments

--- lua/plugins/ui/code.lua
+++ lua/plugins/ui/code.lua
@@ -5,10 +5,20 @@ return {
     main = 'ibl',
     opts = {},
   },
-  { -- Highlight todo, notes, etc in comments
+  { -- Finds and lists all of the TODO, HACK, BUG, etc comment
+    -- in your project and loads them into a browsable list.
     'folke/todo-comments.nvim',
-    event = 'VimEnter',
-    dependencies = { 'nvim-lua/plenary.nvim' },
-    opts = { signs = false },
+    cmd = { 'TodoTrouble', 'TodoTelescope' },
+    lazy = true,
+    opts = {},
+    -- stylua: ignore
+    keys = {
+      { "]t", function() require("todo-comments").jump_next() end, desc = "Next Todo Comment" },
+      { "[t", function() require("todo-comments").jump_prev() end, desc = "Previous Todo Comment" },
+      { "<leader>xt", "<cmd>Trouble todo toggle<cr>", desc = "Todo (Trouble)" },
+      { "<leader>xT", "<cmd>Trouble todo toggle filter = {tag = {TODO,FIX,FIXME}}<cr>", desc = "Todo/Fix/Fixme (Trouble)" },
+      { "<leader>st", "<cmd>TodoTelescope<cr>", desc = "Todo" },
+      { "<leader>sT", "<cmd>TodoTelescope keywords=TODO,FIX,FIXME<cr>", desc = "Todo/Fix/Fixme" },
+    },
   },
 }

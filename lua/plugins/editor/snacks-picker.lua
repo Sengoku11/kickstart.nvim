156a710917c7feb26850f0d7f2f9f694fb004395
Sengoku11 <batyr.sengoku@gmail.com>
2025-09-25 19:38:04 +0800

    chore: picker keymap

--- lua/plugins/editor/snacks-picker.lua
+++ lua/plugins/editor/snacks-picker.lua
@@ -61,9 +61,9 @@ return {
       { "<leader>sM", function() Snacks.picker.man() end, desc = "Man Pages" },
       { "<leader>sp", function() Snacks.picker.lazy() end, desc = "Search for Plugin Spec" },
       { "<leader>sq", function() Snacks.picker.qflist() end, desc = "Quickfix List" },
-      { "<leader>sR", function() Snacks.picker.resume() end, desc = "Resume" },
+      { "<leader>s.", function() Snacks.picker.resume() end, desc = "Repeat" },
       { "<leader>su", function() Snacks.picker.undo() end, desc = "Undo History" },
-      { "<leader>uC", function() Snacks.picker.colorschemes() end, desc = "Colorschemes" },
+      { "<leader>uc", function() Snacks.picker.colorschemes() end, desc = "Colorschemes" },
       -- LSP
       { "gd", function() Snacks.picker.lsp_definitions() end, desc = "Goto Definition" },
       { "gD", function() Snacks.picker.lsp_declarations() end, desc = "Goto Declaration" },

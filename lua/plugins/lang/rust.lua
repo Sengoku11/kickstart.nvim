31af2f9e675802134d4919893ade6ad1b60c2e59
Sengoku11 <batyr.sengoku@gmail.com>
2025-10-18 21:18:17 +0800

    chore: load mason lazily

--- lua/plugins/lang/rust.lua
+++ lua/plugins/lang/rust.lua
@@ -33,6 +33,7 @@ return {
   {
     'mason-org/mason.nvim',
     optional = true,
+    event = 'VeryLazy',
     opts = function(_, opts)
       opts.ensure_installed = opts.ensure_installed or {}
       vim.list_extend(opts.ensure_installed, { 'codelldb' })

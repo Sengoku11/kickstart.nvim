83fa6ea8e7a963bbc3cc088094802c7443c0beee
Sengoku11 <batyr.sengoku@gmail.com>
2025-10-19 11:39:50 +0800

    feat: trouble.nvim

--- lua/plugins/ui/code.lua
+++ lua/plugins/ui/code.lua
@@ -37,4 +37,22 @@ return {
       { "<leader>sT", "<cmd>TodoTelescope keywords=TODO,FIX,FIXME<cr>", desc = "Search Todo/Fix/Fixme" },
     },
   },
+  {
+    'folke/trouble.nvim',
+    event = 'VeryLazy',
+    opts = {}, -- for default options, refer to the configuration section for custom setup.
+    cmd = 'Trouble',
+    -- stylua: ignore
+    keys = {
+      { '<leader>cs', '<cmd>Trouble symbols toggle focus=false<cr>', desc = 'Symbols (Trouble)' },
+      { '<leader>xx', '<cmd>Trouble diagnostics toggle<cr>', desc = 'Diagnostics (Trouble)' },
+      { '<leader>xX', '<cmd>Trouble diagnostics toggle filter.buf=0<cr>', desc = 'Buffer Diagnostics (Trouble)' },
+      { '<leader>xL', '<cmd>Trouble loclist toggle<cr>', desc = 'Location List (Trouble)' },
+      { '<leader>xQ', '<cmd>Trouble qflist toggle<cr>', desc = 'Quickfix List (Trouble)' },
+      { '<leader>cl',
+        '<cmd>Trouble lsp toggle focus=false win.position=right<cr>',
+        desc = 'LSP Definitions / references / ... (Trouble)',
+      },
+    },
+  },
 }

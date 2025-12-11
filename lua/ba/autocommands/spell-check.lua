68aa1c55a49d36548da0a880a9219379fb07fcda
Sengoku11 <batyr.sengoku@gmail.com>
2025-12-11 13:54:48 +0800

    feat: spell file

--- lua/ba/autocommands/spell-check.lua
+++ lua/ba/autocommands/spell-check.lua
@@ -1,5 +1,6 @@
 vim.opt.spell = true
 vim.opt.spelllang = { 'en_us' }
+vim.opt.spellfile = vim.fn.stdpath 'config' .. '/spell/en.utf-8.add'
 
 -- vim.keymap.set('n', '<leader>sn', ']s', { desc = 'Next spelling error' })
 -- vim.keymap.set('n', '<leader>sp', '[s', { desc = 'Prev spelling error' })

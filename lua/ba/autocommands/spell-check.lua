aa171bd6416392044dcf4d317c441dbc4baee486
Sengoku11 <batyr.sengoku@gmail.com>
2025-12-11 18:52:57 +0800

    chore: more spell words and add camel case to config

--- lua/ba/autocommands/spell-check.lua
+++ lua/ba/autocommands/spell-check.lua
@@ -1,6 +1,7 @@
 vim.opt.spell = true
 vim.opt.spelllang = { 'en_us' }
 vim.opt.spellfile = vim.fn.stdpath 'config' .. '/spell/en.utf-8.add'
+vim.opt.spelloptions = 'camel'
 
 -- vim.keymap.set('n', '<leader>sn', ']s', { desc = 'Next spelling error' })
 -- vim.keymap.set('n', '<leader>sp', '[s', { desc = 'Prev spelling error' })

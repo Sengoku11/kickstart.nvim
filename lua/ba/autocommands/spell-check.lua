d8eaeb6f6283614c225a9ca2cb9bf55c19e15877
Sengoku11 <batyr.sengoku@gmail.com>
2025-12-11 20:51:11 +0800

    feat: local and global spell files

--- lua/ba/autocommands/spell-check.lua
+++ lua/ba/autocommands/spell-check.lua
@@ -1,7 +1,21 @@
 vim.opt.spell = true
 vim.opt.spelllang = { 'en_us' }
-vim.opt.spellfile = vim.fn.stdpath 'config' .. '/spell/en.utf-8.add'
 vim.opt.spelloptions = 'camel'
+
+-- Global dev spellfile (versioned in dotfiles repo)
+local global_spell = vim.fn.stdpath 'config' .. '/spell/en.utf-8.add'
+
+-- Local machine-wide spellfile
+local local_spell_dir = vim.fn.stdpath 'data' .. '/spell'
+if vim.fn.isdirectory(local_spell_dir) == 0 then
+  vim.fn.mkdir(local_spell_dir, 'p')
+end
+local local_spell = local_spell_dir .. '/local.en.utf-8.add'
+
+-- Order matters:
+--   1st = local (machine) → `zg`
+--   2nd = global (synced) → `2zg`
+vim.opt.spellfile = { local_spell, global_spell }
 
 -- vim.keymap.set('n', '<leader>sn', ']s', { desc = 'Next spelling error' })
 -- vim.keymap.set('n', '<leader>sp', '[s', { desc = 'Prev spelling error' })

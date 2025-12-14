f0b64293b8c754ef946d61566e6c99f593347bab
Sengoku11 <batyr.sengoku@gmail.com>
2025-12-14 12:48:52 +0800

    chore: make harper config more generic

--- lua/plugins/lang/grammar.lua
+++ lua/plugins/lang/grammar.lua
@@ -1,40 +1,40 @@
 return {
   {
     'neovim/nvim-lspconfig',
-    opts = function(_, opts)
-      opts.servers = opts.servers or {}
-
-      opts.servers.harper_ls = vim.tbl_deep_extend('force', opts.servers.harper_ls or {}, {
-        settings = {
-          ['harper-ls'] = {
-            userDictPath = '',
-            workspaceDictPath = '',
-            fileDictPath = '',
-            linters = {
-              SpellCheck = false,
-              ToDoHyphen = false,
-              ExpandTimeShorthands = false,
-              SentenceCapitalization = false,
-              SpelledNumbers = true,
-              NoOxfordComma = false,
-              BoringWords = true,
-              UnclosedQuotes = true,
-              LongSentences = true,
-              Spaces = true,
-              Matcher = true,
-              CorrectNumberSuffix = true,
+    opts = {
+      servers = {
+        harper_ls = {
+          settings = {
+            ['harper-ls'] = {
+              userDictPath = '',
+              workspaceDictPath = '',
+              fileDictPath = '',
+              linters = {
+                SpellCheck = false,
+                ToDoHyphen = false,
+                ExpandTimeShorthands = false,
+                SentenceCapitalization = false,
+                SpelledNumbers = true,
+                NoOxfordComma = false,
+                BoringWords = true,
+                UnclosedQuotes = true,
+                LongSentences = true,
+                Spaces = true,
+                Matcher = true,
+                CorrectNumberSuffix = true,
+              },
+              codeActions = { ForceStable = false },
+              markdown = { IgnoreLinkTitle = false },
+              diagnosticSeverity = 'hint',
+              isolateEnglish = false,
+              dialect = 'American',
+              maxFileLength = 120000,
+              ignoredLintsPath = '',
+              excludePatterns = {},
             },
-            codeActions = { ForceStable = false },
-            markdown = { IgnoreLinkTitle = false },
-            diagnosticSeverity = 'hint',
-            isolateEnglish = false,
-            dialect = 'American',
-            maxFileLength = 120000,
-            ignoredLintsPath = '',
-            excludePatterns = {},
           },
         },
-      })
-    end,
+      },
+    },
   },
 }

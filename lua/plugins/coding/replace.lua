8026da4eae212640d02cf90224993bd7da099598
Sengoku11 <batyr.sengoku@gmail.com>
2025-12-20 18:20:30 +0800

    feat(grug): replace with ripgrep

---
+++ lua/plugins/coding/replace.lua
@@ -0,0 +1,20 @@
+return {
+  {
+    'MagicDuck/grug-far.nvim',
+    event = 'VeryLazy',
+    -- Note (lazy loading): grug-far.lua defers all it's requires so it's lazy by default
+    -- additional lazy config to defer loading is not really needed...
+    config = function()
+      -- optional setup call to override plugin options
+      -- alternatively you can set options with vim.g.grug_far = { ... }
+      require('grug-far').setup {
+        -- options, see Configuration section below
+        -- there are no required options atm
+      }
+
+      vim.keymap.set({ 'n', 'x' }, '<leader>sr', function()
+        require('grug-far').open { visualSelectionUsage = 'operate-within-range' }
+      end, { desc = 'grug-far: Search within range' })
+    end,
+  },
+}

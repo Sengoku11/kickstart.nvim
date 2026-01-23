a334e0c26a2b551da72c6a62cc1f28b188e2a558
Sengoku11 <batyr.sengoku@gmail.com>
2026-01-23 22:13:58 +0800

    chore: update daml config
    test: value
    
    test: value

--- lua/plugins/lang/daml.lua
+++ lua/plugins/lang/daml.lua
@@ -1,3 +1,16 @@
 return {
-  { 'Sengoku11/daml.nvim', branch = 'main', ft = 'daml', opts = {} },
+  {
+    'Sengoku11/daml.nvim',
+    branch = 'dev',
+    ft = 'daml',
+    keys = {
+      { '<leader>gt', '<cmd>DamlRunScript<cr>', desc = 'Run Daml Script' },
+    },
+    opts = {
+      -- daml_script = { render = false },
+      lsp = {
+        cmd = { 'dpm', 'damlc', 'multi-ide', '--RTS', '+RTS', '-M4G', '-N' },
+      },
+    },
+  },
 }

cbdfe837d698d6f7b17f99b8c4866e0b796a42fd
Sengoku11 <batyr.sengoku@gmail.com>
2026-02-17 19:55:12 +0800

    chore(git): attach hunks to untracked files

--- lua/plugins/editor/git.lua
+++ lua/plugins/editor/git.lua
@@ -55,6 +55,7 @@ return {
     'lewis6991/gitsigns.nvim',
     event = 'VeryLazy',
     opts = {
+      attach_to_untracked = true,
       signs = {
         add = { text = '+' },
         change = { text = '~' },
@@ -100,7 +101,6 @@ return {
         map('n', '<leader>hs', gitsigns.stage_hunk, { desc = 'Stage Hunk' })
         map('n', '<leader>hr', gitsigns.reset_hunk, { desc = 'Reset Hunk' })
         map('n', '<leader>hS', gitsigns.stage_buffer, { desc = 'Stage Buffer' })
-        map('n', '<leader>hu', gitsigns.stage_hunk, { desc = 'Undo Stage Hunk' })
         map('n', '<leader>hR', gitsigns.reset_buffer, { desc = 'Reset Buffer' })
         map('n', '<leader>hh', gitsigns.preview_hunk, { desc = 'Hunk Hovered' })
         map('n', '<leader>hi', gitsigns.preview_hunk_inline, { desc = 'Hunk Inline' })
